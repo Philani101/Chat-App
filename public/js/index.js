5fe71a217fcfa869af25302febc5ddab332851b0
Leo:) <philanimkalipi@gmail.com>
2025-04-29 19:35:40 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,6 @@ socket.on('connect', function ()  {
 socket.on('newMessage' , function (message){
     console.log('newMessage: ', message); //this will log the message to the console when the newMessage event is emitted from the server
                        //you can also use this to update the UI with the new message
-    let 
     alert("message received from server" );
 }); //listens for newMessage event from server
 
